603eea12159ce8c688d141a79cf188972e38a558
Bhargav Suhagiya <bhargav@Bhargavs-MacBook-Air.local>
2025-12-11 00:12:16 +0530

    fix: lock node version to 20.x and restore privileged intents

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        // GatewayIntentBits.MessageContent,
-        // GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ],
 });
 
